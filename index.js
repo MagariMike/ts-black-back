28b5764c71e7059a354ca1d716deea7743134368
Mike Brewer <michaelandrewbrewer@gmail.com>
2023-07-22 17:42:13 +0100

    ts refactoring

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 var firstCard = 10;
 var secondCard = 4;
+var cards = [firstCard, secondCard];
 var sum = firstCard + secondCard;
 var isAlive = true;
 var message = "";
 var messageEl = document.getElementById("message-el");
 var sumEl = document.getElementById("sum-el");
 var cardsEl = document.getElementById("cards-el");
-var startGame = function () {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+var renderGame = function () {
+    cardsEl.textContent = "Cards: ".concat(cards[0], " + ").concat(cards[1]);
     sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
+        console.log(message);
     }
     else if (sum === 21) {
         message = "You've got Blackjack!";
+        console.log(message);
     }
     else {
         message = "You're out of the game!";
+        console.log(message);
         isAlive = false;
     }
 };
 var newCard = function () {
-    console.log("Drawing a new card from the deck!");
+    var card = 7;
+    sum += card;
+    renderGame();
 };
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 let firstCard = 10; 
 let secondCard = 4;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let isAlive = true; 
 let message = "";
@@ -7,20 +8,25 @@ let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el")!;
 let cardsEl = document.getElementById("cards-el")!; 
 
-const startGame = () => { 
-	cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+const renderGame = () => { 
+	cardsEl.textContent = `Cards: ${cards[0]} + ${cards[1]}`;
 	sumEl.textContent = "Sum: " + sum;
 	if (sum <= 20) {
 		message = "Do you want to draw a new card?";
+		console.log(message)
 	} else if (sum === 21) {
 		message = "You've got Blackjack!";
+		console.log(message)
 	} else {
 		message = "You're out of the game!";
+		console.log(message)
 		isAlive = false;
 	}
 }
 
 
 const newCard = () => {
-	console.log("Drawing a new card from the deck!")
+	let card = 7;
+	sum += card; 
+	renderGame()
 }
